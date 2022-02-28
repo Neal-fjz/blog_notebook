bea3b6508a15107644bed52beb20b7769d9804e6
linneal <linhejun@live.com>
2022-03-01 06:50:39 +0800

    0.0.2

--- user-service/src/main/java/com/opensource/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/opensource/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.userservice;
+package com.opensource.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- user-service/src/test/java/com/opensource/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/opensource/userservice/UserServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.userservice;
+package com.opensource.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
